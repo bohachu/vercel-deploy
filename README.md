7e04c213f6b8c00fd5ae2b5f11ee7977aed3f519
chiubowen <cbh@cameo.tw>
2023-04-05 22:29:05 +0800

    == 2023-04-05 22:28 Bowen == . 已經可以前端顯示 . 準備要弄上 vercel 測試

--- README.md
+++ README.md
@@ -1,32 +1,25 @@
-# FastAPI on Vercel Example
+# 提供前端可以讓建立 github 專案與部署 vercel 完全自動化
+2023-04-05 Bowen Chiu
 
-This is [FastAPI](https://fastapi.tiangolo.com/) example app deployed on [Vercel](https://vercel.com/).
+###
 
-### Requirements
-
--   Vercel account
--   [Vercel CLI](https://vercel.com/cli)
-
-### Setup
-
-1. `git clone && cd fastapi-vercel`
-2. _(Recommended)_ Create a virtual environment and activate it
-
-```bash
-python -m venv env
-
-source env/bin/activate
-```
-
-3. Install dependencies
-
-```bash
-pip install -r requirements.txt
-```
-
-4. Deploy to Vercel
-
-```bash
-vercel
-```
-
+. 請幫我建構前後端，都使用 fastapi app.py 單一檔案不要有額外的檔案出現
+. 前端使用 bootstrap RWD 排版要整齊, 手機也要能用, 引用 bootstrap 時不要 integrity 屬性
+. 前端要能輸入以下參數 001 to 007
+    . 環境變數
+        001 <your-github-access-token>
+        002 <your-vercel-access-token>
+    . git_clone 
+        003 <source_url> 
+        004 <target_name>
+    . vercel_deploy
+        005 <github_repo_url> 
+        006 <vercel_project_name> 
+        007 <vercel_team_id>
+. 前端參數輸入之後儲存在瀏覽器的 localStorage
+. 如果 localStorage 沒有這些參數就預設是空的輸入框
+. 如果 localStorage 已經有 001 to 007 參數，事先帶入預設值放上去
+. 前端要有一個「送出」按鈕，可以把參數送給後端
+. 前端要有一個較大塊的返回顯示區域，專門用來顯示後端執行之後的返回結果
+. 送出按鈕按下去之後，要能夠把後端返回哪些成功或失敗的訊息，全部顯示在返回顯示區域
+. fastapi 後端接收到 001 to 007 參數之後，返回一個 json 說明 OK 成功
--- app.py
+++ app.py
@@ -1,41 +1,30 @@
-from datetime import datetime
-from typing import Optional
-import httpx
-from fastapi import FastAPI, HTTPException, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
-import models
+from pydantic import BaseModel
+from starlette.requests import Request
+from starlette.responses import HTMLResponse
 
 app = FastAPI()
-
 templates = Jinja2Templates(directory="templates")
 
 
+class Params(BaseModel):
+    github_access_token: str
+    vercel_access_token: str
+    source_url: str
+    target_name: str
+    github_repo_url: str
+    vercel_project_name: str
+    vercel_team_id: str
+
+
+@app.post("/submit")
+async def submit_params(params: Params):
+    # Process the submitted parameters (e.g., clone the GitHub repository, deploy to Vercel, etc.)
+    # For now, just return a success message
+    return {"message": "123 Parameters received successfully", "params": params}
+
+
 @app.get("/", response_class=HTMLResponse)
-def index(request: Request, username: str = None):
-    if not username:
-        return templates.TemplateResponse("index.html", context={"request": request})
-
-    user = get_github_profile(request, username)
-
-    context = {"request": request, "user": user}
-
-    return templates.TemplateResponse("index.html", context=context)
-
-
-@app.get("/{username}", response_model=models.GithubUserModel)
-def get_github_profile(request: Request, username: str) -> Optional[models.GithubUserModel]:
-
-    headers = {"accept": "application/vnd.github.v3+json"}
-
-    response = httpx.get(f"https://api.github.com/users/{username}", headers=headers)
-
-    if response.status_code == 404:
-        return False
-
-    user = models.GithubUserModel(**response.json())
-
-    # Sobreescribir la fecha con el formato que necesitamos
-    user.created_at = datetime.strptime(user.created_at, "%Y-%m-%dT%H:%M:%SZ").strftime("%d/%m/%y")
-
-    return user
+async def main(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
